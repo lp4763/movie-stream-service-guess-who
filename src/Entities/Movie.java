80a34907d7b2f90dc34136436c44aa37d92c0371
Luke Pais <lp4763@rit.edu>
2021-10-28 19:47:02 -0400

    added movie length and release date

--- src/Entities/Movie.java
+++ src/Entities/Movie.java
@@ -4,17 +4,21 @@ import java.util.Date;
 
 public class Movie {
     private String title;
+    private int length;
     private String studio;
     private String genre;
+    private String releaseDate;
     private int userRatingCount;
     private float ratingAvgScore;
     private String mpaaRating;
     private int movieId;
-    public Movie(String title, String  studio, String genre, int userRatingCount, float ratingAvgScore,
-                 String mpaaRating, int movieId){
+    public Movie(String title, int length, String  studio, String genre, String releaseDate,
+                 int userRatingCount, float ratingAvgScore, String mpaaRating, int movieId){
         this.title = title;
+        this.length = length;
         this.studio = studio;
         this.genre = genre;
+        this.releaseDate = releaseDate;
         this.userRatingCount = userRatingCount;
         this.ratingAvgScore = ratingAvgScore;
         this.mpaaRating = mpaaRating;
@@ -24,11 +28,20 @@ public class Movie {
     public String getTitle() {
         return title;
     }
+
+    public int getLength() {
+        return length;
+    }
+
     public String getGenre() {
         return genre;
     }
     public String getStudio() {
         return studio;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
     }
 
     public int getUserRatingCount() {
@@ -51,8 +64,10 @@ public class Movie {
     public String toString() {
         return "Movie{" +
                 "title='" + title + '\'' +
+                ", length=" + length +
                 ", studio='" + studio + '\'' +
                 ", genre='" + genre + '\'' +
+                ", releaseDate='" + releaseDate + '\'' +
                 ", userRatingCount=" + userRatingCount +
                 ", ratingAvgScore=" + ratingAvgScore +
                 ", mpaaRating='" + mpaaRating + '\'' +
